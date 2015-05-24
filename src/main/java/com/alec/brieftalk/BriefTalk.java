362ff9ab37f1b562906318f4e411b18e298eb889
Alec <ae40515@yahoo.com.tw>
2015-05-24 19:57:01 +0800

    Add login screen Change look and feel

--- src/main/java/com/alec/brieftalk/BriefTalk.java
+++ src/main/java/com/alec/brieftalk/BriefTalk.java
@@ -1,10 +1,19 @@
 package com.alec.brieftalk;
 
+import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
 /**
  * Created by Alec on 2015/5/24.
  */
 public class BriefTalk {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        } catch (Exception e) {
+
+        }
+
 
     }
 }
---
+++ src/main/java/com/alec/brieftalk/LoginWindow.java
@@ -0,0 +1,14 @@
+package com.alec.brieftalk;
+
+import javax.swing.*;
+
+/**
+ * Created by Alec on 2015/5/24.
+ */
+public class LoginWindow {
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton loginButton;
+}
